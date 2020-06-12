b17d831298bc9875a1e11933c05db5ec74ec1af6
Patrick Hennessy <phennessy517@gmail.com>
2020-06-12 01:11:52 -0500

    Fixes for automation. Remove spot instances, add sg rules for grafana (#9)

--- gameservers/launch_templates.tf
+++ gameservers/launch_templates.tf
@@ -1,3 +1,10 @@
+locals {
+    userdata = <<EOF
+#!/bin/bash -v
+/opt/startup.sh
+EOF
+}
+
 data "aws_ami" "tf2_competitive" {
     most_recent = true
 
@@ -16,20 +23,16 @@ resource "aws_launch_template" "tf2_competitive" {
     image_id = "${data.aws_ami.tf2_competitive.id}"
     instance_type = "${var.gameserver_instance_size}"
     key_name = "${var.ssh_keyname}"
-    user_data = "${base64encode(<<EOF
-		#! /bin/bash
-        /bin/bash /opt/startup.sh
-	EOF
-    )}"
+    user_data = "${base64encode(local.userdata)}"
     
-    instance_market_options {
-        market_type = "spot"
-        spot_options {
-            block_duration_minutes = "360"
-            spot_instance_type = "one-time"
-            instance_interruption_behavior = "terminate"
-        }
-    }
+    # instance_market_options {
+    #     market_type = "spot"
+    #     spot_options {
+    #         block_duration_minutes = "360"
+    #         spot_instance_type = "one-time"
+    #         instance_interruption_behavior = "terminate"
+    #     }
+    # }
 
     # Network
     network_interfaces {
