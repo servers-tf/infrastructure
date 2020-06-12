d6b7e5f39fada3dad629e71fd26579aeea103816
Patrick Hennessy <phennessy517@gmail.com>
2020-06-12 01:03:51 -0500

    Fixes for automation. Remove spot instances, add sg rules for grafana

--- automation/clean_amis.tf
+++ automation/clean_amis.tf
@@ -5,27 +5,39 @@ variable "clean_amis" {
     ]
 }
 
+#-------------------------------------------------------------------------------
+# IAM
+#-------------------------------------------------------------------------------
 resource "aws_iam_role" "clean_amis" {
-    name = "iam_for_lambda"
+    name = "lambda_clean_amis"
     path = "/"
     assume_role_policy = file("${path.module}/policies/sts_assumerole.json")
 }
-
 resource "aws_iam_role_policy" "allow_image_delete" {
     name = "allow_image_delete"
     role = "${aws_iam_role.clean_amis.id}"
 
     policy = file("${path.module}/policies/lambda_deleteamis.json")
 }
+resource "aws_iam_role_policy" "allow_cloudwatch_logs" {
+    name = "allow_logs"
+    role = "${aws_iam_role.clean_amis.id}"
 
-resource "aws_s3_bucket_object" "clean_amis_code" {
-    bucket = "${aws_s3_bucket.lambdas.id}"
-    key = "clean_amis.zip"
-    source = "${path.module}/clean_amis.zip"
-
-    etag = "${filemd5("${path.module}/clean_amis.zip")}"
+    policy = file("${path.module}/policies/lambda_cloudwatchlogs.json")
 }
 
+#-------------------------------------------------------------------------------
+# CloudWatch Logs
+#-------------------------------------------------------------------------------
+resource "aws_cloudwatch_log_group" "this" {
+    count = "${length(var.clean_amis)}"
+    name = "/aws/lambda/${aws_lambda_function.clean_amis[count.index].function_name}"
+    retention_in_days = 60
+}
+
+#-------------------------------------------------------------------------------
+# Function
+#-------------------------------------------------------------------------------
 resource "aws_lambda_function" "clean_amis" {
     count = "${length(var.clean_amis)}"
     
@@ -50,6 +62,20 @@ resource "aws_lambda_function" "clean_amis" {
     }
 }
 
+#-------------------------------------------------------------------------------
+# Code in S3
+#-------------------------------------------------------------------------------
+resource "aws_s3_bucket_object" "clean_amis_code" {
+    bucket = "${aws_s3_bucket.lambdas.id}"
+    key = "clean_amis.zip"
+    source = "${path.module}/clean_amis.zip"
+
+    etag = "${filemd5("${path.module}/clean_amis.zip")}"
+}
+
+#-------------------------------------------------------------------------------
+# Trigger
+#-------------------------------------------------------------------------------
 resource "aws_cloudwatch_event_rule" "run_daily" {
     name = "run_daily"
     schedule_expression = "rate(1 day)"
